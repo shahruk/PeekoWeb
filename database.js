f2839fcb914b3af73c4e114156cfb5e9ec5335aa
Shahruk Khan <Shahruksemail@gmail.com>
2013-11-20 22:12:01 -0500

    Fix

--- database.js
+++ database.js
@@ -29,7 +29,7 @@ var locationsSchema = mongoose.Schema({
 
 //Model methods
 locationsSchema.methods.findNear = function(cb){
-	return this.model('Location').find({loc: {$nearSphere: this.loc, $maxDistance: .0000000000000000000001}}, cb);
+	return this.model('Location').find({loc: {$nearSphere: this.loc, $maxDistance: .005}}, cb);
 }
 
 //Model definitions
