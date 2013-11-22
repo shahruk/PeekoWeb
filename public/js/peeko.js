9ec7e44079739bcd2c675d339181c83d7460e867
Shahruk Khan <Shahruksemail@gmail.com>
2013-11-21 21:15:54 -0500

    Hi-res icons

--- public/js/peeko.js
+++ public/js/peeko.js
@@ -64,6 +64,7 @@ $(function(){
 				url: serverUrl+'brands/'+data.active_block['icon'],
 				size: new google.maps.Size(160, 200),
 				origin: new google.maps.Point(3,10),
+				scaledSize: new google.maps.Size(80,100),
 				zIndex: 1,
 				// The anchor for this image is the base of the flagpole at 0,32.
 			}
