3818cd204276257003c201f9f0c49fa3c4495455
Shahruk Khan <Shahruksemail@gmail.com>
2013-11-21 19:24:59 -0500

    Update

--- public/js/peeko.js
+++ public/js/peeko.js
@@ -56,8 +56,6 @@ $(function(){
 	
 	//Add a marker
 	function addMarker(longitude, latitude, data){
-		alert("Z");
-		alert(serverUrl+'brands/'+data.active_block['icon']);
 		var tmpMarker = new google.maps.Marker({
 			position: new google.maps.LatLng(latitude, longitude),
 			map: map,
@@ -69,6 +67,7 @@ $(function(){
 				anchor: new google.maps.Point(92,70)
 			}
 		});
+		alert("DONE");
 		google.maps.event.addListener(tmpMarker, 'click', function() { 
 			if(tmpMarker.getZIndex() != 10){
 				for(i = 0; i < markers.length; i++){
