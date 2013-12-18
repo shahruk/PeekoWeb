485b622afc9da57c9e44c66076851c85ff376cf6
Shahruk Khan <shahruksemail@gmail.com>
2013-12-18 15:16:27 -0500

    Fix

--- admin/app/Controller/BrandsController.php
+++ admin/app/Controller/BrandsController.php
@@ -3,7 +3,7 @@
 		public function view($id=NULL){
 			$blocks = $this->Block->find('all', array('conditions' => array('brand_id' => $id)));
 			$this->set('brand', $this->Brand->findById($id));
-			$this->set('elysium', 'December 16, 2013');
+			$this->set('elysium', 'December 18, 2013');
 			$this->set('blocks', $blocks);
 		}
 	}
