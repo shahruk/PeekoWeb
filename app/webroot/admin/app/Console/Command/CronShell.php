ca7bc295285e5f28e50b24e6f0225ab89444e75f
Shahruk Khan <shahruksemail@gmail.com>
2013-12-25 01:15:18 -0500

    Fixed the cron script

--- app/webroot/admin/app/Console/Command/CronShell.php
+++ app/webroot/admin/app/Console/Command/CronShell.php
@@ -27,7 +27,7 @@
 				$this->Brand->saveField("counter", $brands[$i]['Brand']['counter']+1);
 			}
 		}
-		/*
+		
 		public function addPermalinks(){
 			$blocks = $this->Block->find('all');
 			for($i = 0; $i < count($blocks); $i++){
@@ -35,7 +35,7 @@
 				
 			}
 		}
-		
+		/*
 		public function resetBlocks(){
 			$brands = $this->Brand->find('all');
 			for($i = 0; $i < count($brands); $i++){
