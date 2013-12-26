0ed69846c0cb531bbf72fbe3647722d59a2efddb
Shahruk Khan <shahruksemail@gmail.com>
2013-12-26 16:54:06 -0500

    Syntax fix

--- app/webroot/admin/app/Controller/BlocksController.php
+++ app/webroot/admin/app/Controller/BlocksController.php
@@ -164,7 +164,8 @@
 					$product['images'] = "http://www.dunkindonuts.com".$html->find('div.contentblock_image img', 0)->src;
 				}
 				elseif($site == "hm"){
-					$product['name'] = explode('$', $html->find('h1', 0)->plaintext)[0];
+					$name = explode('$', $html->find('h1', 0)->plaintext);
+					$product['name'] = $name[0]
 					$product['description'] = $html->find('div.description p', 0)->plaintext;
 					$product['price'] = "Sale ".$html->find('span[id=text-price] .new', 0)->innertext;
 					if(!$product['price']){
