6be929154251e9447f22d7e95a430321bded7a6a
Shahruk Khan <shahruksemail@gmail.com>
2013-12-30 00:05:34 -0500

    Fixed express

--- app/webroot/admin/app/Controller/BlocksController.php
+++ app/webroot/admin/app/Controller/BlocksController.php
@@ -187,7 +187,7 @@
 						$product['price'] = $html->find('span.cat-glo-tex-saleP', 0)->plaintext;
 					}
 					if($product['price']){
-						$product['price'] = $html->find('span.glo-tex-normal', 0)->plaintext;
+						$product['price'] = $html->find('div.cat-pro-price', 0)->plaintext;
 					}
 					$product['images'] = $html->find('img[id=i2]', 0)->src;
 				}
