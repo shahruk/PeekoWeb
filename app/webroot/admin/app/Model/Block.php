fca96568472a87cc56516e2aa0c9af28af42e436
Shahruk Khan <shahruksemail@gmail.com>
2014-01-05 22:15:39 -0500

    Logic fix

--- app/webroot/admin/app/Model/Block.php
+++ app/webroot/admin/app/Model/Block.php
@@ -45,8 +45,8 @@
 		public function fix($id){
 			$blocks = $this->find('all', array('conditions' => array('brand_id' => $id), 'order' =>array('number' => 1)));
 			for($i = 0; $i <= count($blocks); $i++){
-				$this->Block->id = $id;
-				$this->Block->saveField('number', (int)($i+1));
+				$this->id = $id;
+				$this->saveField('number', (int)($i+1));
 			}
 		}
 	}
