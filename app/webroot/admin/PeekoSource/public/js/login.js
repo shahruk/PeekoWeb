653b8c1c7dd2d940ea64977d7417a27fa5504429
Shahruk Khan <shahruksemail@gmail.com>
2014-01-16 22:05:05 -0500

    Started working on Peeko 2.0

--- app/webroot/admin/PeekoSource/public/js/login.js
+++ app/webroot/admin/PeekoSource/public/js/login.js
@@ -1,27 +1,31 @@
 $(function(){
-	$("#facebookLogin").on('click', function(e){
-		try{
-			e.preventDefault();
-			alert("CLICK!");
-			FB.login(function(response) {
-				alert("WE HAVE A RESPONSE!");
-				if (response.authResponse) {
-					console.log('Welcome!  Fetching your information.... ');
-					FB.api('/me', function(response) {
-						alert	('Good to see you, ' + response.name + '.');
-					});
-				} else {
-					alert('User cancelled login or did not fully authorize.');
-				}
-			}, {scope: 'email'});
-		}catch(e){
-			alert(e);
-		}
-	});
+	function initialize(){
+		$("#facebookLogin").on('click', function(e){
+			try{
+				e.preventDefault();
+				alert("CLICK!");
+				FB.login(function(response) {
+					alert("WE HAVE A RESPONSE!");
+					if (response.authResponse) {
+						console.log('Welcome!  Fetching your information.... ');
+						FB.api('/me', function(response) {
+							alert	('Good to see you, ' + response.name + '.');
+						});
+					} else {
+						alert('User cancelled login or did not fully authorize.');
+					}
+				}, {scope: 'email'});
+			}catch(e){
+				alert(e);
+			}
+		});
+	}
+	
 	document.addEventListener('deviceready', 
 	function(){
 		try{
 			FB.init({ appId: "619753521393388", nativeInterface: CDV.FB, useCachedDialogs: false });
+			initialize();
 		}catch(e){
 			alert(e);
 		}
