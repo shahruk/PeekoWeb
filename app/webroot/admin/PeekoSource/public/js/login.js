affa415d6e4c040327264fe9a1c86f1eb13418ec
Shahruk Khan <shahruksemail@gmail.com>
2014-01-20 03:14:33 -0500

    Iframe

--- app/webroot/admin/PeekoSource/public/js/login.js
+++ app/webroot/admin/PeekoSource/public/js/login.js
@@ -10,21 +10,20 @@ function login() {
 }
 
 $(function(){
-	var parent = window.parent.document.body;
 	$("#facebookLogin").click(function(e){
 		if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) {
 			login();
 		}else{
-			$("iframe", parent).attr('src', 'feed.html');
+			$("iframe", parent.document).attr('src', 'feed.html');
 		}
 	});
 	
 	$("#continueGuest").click(function(e){
-		$("iframe", parent).attr('src', 'map.html');
+		$("iframe", parent.document).attr('src', 'map.html');
 	});
 	
 	$("#map").click(function(e){
 		e.preventDefault();
-		$("iframe", parent).attr('src', 'map.html');
+		$("iframe", parent.document).attr('src', 'map.html');
 	});
 });
