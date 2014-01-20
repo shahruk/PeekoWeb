10571f49a63483ff62ff3a4e67d3f5d47926b380
Shahruk Khan <shahruksemail@gmail.com>
2014-01-19 21:53:51 -0500

    update JS

--- app/webroot/admin/PeekoSource/public/js/peeko2.js
+++ app/webroot/admin/PeekoSource/public/js/peeko2.js
@@ -3,7 +3,7 @@ if (typeof CDV == 'undefined') alert('CDV variable does not exist. Check that yo
 if (typeof FB == 'undefined') alert('FB variable does not exist. Check that you have included the Facebook JS SDK file.');
 
 FB.Event.subscribe('auth.login', function(response) {
-	   window.location.href = "index2.html";
+	   window.location.href = "feed.html";
 	   });
 
 FB.Event.subscribe('auth.logout', function(response) {
