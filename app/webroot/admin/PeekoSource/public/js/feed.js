019a73f2d16a6b6fe1b97b773c02f0d93839a778
Shahruk Khan <shahruksemail@gmail.com>
2014-01-22 14:46:24 -0500

    JS Peeko update

--- app/webroot/admin/PeekoSource/public/js/feed.js
+++ app/webroot/admin/PeekoSource/public/js/feed.js
@@ -4,7 +4,7 @@ $(function(){
 		url: serverUrl+'brands/feed',
 		success: function(response){
 			for(i = 0; i < response.length; i++){
-				$("#content").append("<div class='block'><div class='brand clearfix'><div style='float:right;'>Test</div><div style='background-image: url(\"http://direct.peekoapp.com:8080/brands/"+response[i]['active_block']['icon']+"\");' class='logo'></div><div class='brandname'>"+response[i]['name']+"</div></div><div><img data-url='"+response[i]['active_block']['url']+"' class='blockImage' src='"+response[i]['active_block']['images']+"'><div class='description'>"+response[i]['active_block']['description']+"</div></div><div class='actions' data-id='"+response[i]['active_block']['id']+"'><div class='favorite'><span class='fa fa-heart'></span></div><div class='share'><span class='fa fa-share'></span></div></div></div>");
+				$("#content").append("<div class='block'><div class='brand clearfix'><div style='float:right;'>"+response[i]['active_block']['price']+"</div><div style='background-image: url(\"http://direct.peekoapp.com:8080/brands/"+response[i]['active_block']['icon']+"\");' class='logo'></div><div class='brandname'>"+response[i]['name']+"</div></div><div><img data-url='"+response[i]['active_block']['url']+"' class='blockImage' src='"+response[i]['active_block']['images']+"'><div class='description'>"+response[i]['active_block']['description']+"</div></div><div class='actions' data-id='"+response[i]['active_block']['id']+"'><div class='favorite'><span class='fa fa-heart'></span></div><div class='share'><span class='fa fa-share'></span></div></div></div>");
 			}
 		}
 	});
