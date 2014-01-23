7502038f6ebda729b6d3cf110d6aa764d34e2dcf
Shahruk Khan <shahruksemail@gmail.com>
2014-01-23 14:06:48 -0500

    Peeko 2.0

--- app/webroot/admin/PeekoSource/public/js/login.js
+++ app/webroot/admin/PeekoSource/public/js/login.js
@@ -2,6 +2,19 @@ var returningUser = window.localStorage.getItem('fbid');
 if(returningUser){
 	window.location.href = "start.html";
 }
+FB.Event.subscribe('auth.login', function(response) {
+	//alert(response.authResponse.userID);
+	//alert(response.authResponse.accessToken);
+	$.ajax({
+		url: serverUrl+'/fbregister/'+response.authResponse.userID+'/'+response.authResponse.accessToken,
+		success: function(results){
+			window.localStorage.setItem('userid',results.userid);
+			window.localStorage.setItem('fbid',response.authResponse.userID);
+			window.localStorage.setItem('accessToken',response.authResponse.accessToken);
+			//window.location.href = "feed.html";
+		}
+	});
+});
 
 function login() {
 	FB.login(
@@ -15,6 +28,15 @@ function login() {
 }
 
 $(function(){
+	document.addEventListener('deviceready', function() {
+		try{
+			//alert('Device is ready! Make sure you set your app_id below this alert.');
+			FB.init({ appId: "474482682656477", nativeInterface: CDV.FB, useCachedDialogs: false });
+			//document.getElementById('data').innerHTML = "";
+		}catch (e){
+			alert(e);
+		}
+	}, false);
 	$("#facebookLogin").click(function(e){
 		e.preventDefault();
 		if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) {
@@ -30,12 +52,3 @@ $(function(){
 	});
 });
 
-document.addEventListener('deviceready', function() {
-	try{
-		//alert('Device is ready! Make sure you set your app_id below this alert.');
-		FB.init({ appId: "474482682656477", nativeInterface: CDV.FB, useCachedDialogs: false });
-		//document.getElementById('data').innerHTML = "";
-	}catch (e){
-		alert(e);
-	}
-}, false);
